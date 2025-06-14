8911f89382b4c002a0cfd3772269a0dbc9b3223b
ElsoMTLV <elsowatches@gmail.com>
2025-06-15 01:27:30 +0300

    Update israelhayom.py

--- sources/israelhayom.py
+++ sources/israelhayom.py
@@ -1,3 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
 
 def get_articles():
-    return [{"title": "Example from israelhayom", "url": "https://example.com/israelhayom"}]
+    url = "https://www.israelhayom.co.il/tag/מכבי-תל-אביב-בכדורסל"
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, "html.parser")
+    items = soup.select("div.compactArticle a.compactArticle_link")
+
+    articles = []
+    for item in items[:5]:
+        title = item.get_text(strip=True)
+        link = "https://www.israelhayom.co.il" + item["href"]
+        articles.append({"title": title, "url": link})
+    return articles
