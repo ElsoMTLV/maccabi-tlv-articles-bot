c25b572b760bf44b062d1c0921f9808c28876a3c
ElsoMTLV <elsowatches@gmail.com>
2025-06-15 00:50:27 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Maccabi TLV Articles Bot
+
+Scrapes basketball news from 5 sources and posts new articles to a Telegram group.
+
+## Setup
+
+1. Add to Render as a Cron Job
+2. Set environment variables:
+   - BOT_TOKEN
+   - CHAT_ID
---
+++ telegram.py
@@ -0,0 +1,7 @@
+
+import requests
+
+def send_article(token, chat_id, article):
+    text = f"{article['title']}\n{article['url']}"
+    requests.post(f"https://api.telegram.org/bot{token}/sendMessage",
+                  data={{"chat_id": chat_id, "text": text}})
