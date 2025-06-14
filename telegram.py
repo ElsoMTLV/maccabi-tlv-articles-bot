18e2114d280c91735e6916282a6aa6e9bdab54fb
ElsoMTLV <elsowatches@gmail.com>
2025-06-15 01:17:10 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -4,4 +4,4 @@ import requests
 def send_article(token, chat_id, article):
     text = f"{article['title']}\n{article['url']}"
     requests.post(f"https://api.telegram.org/bot{token}/sendMessage",
-                  data={{"chat_id": chat_id, "text": text}})
+                  data={"chat_id": chat_id, "text": text})
