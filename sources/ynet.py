c3d5b18c9af74058b606d9d08a2c8e1cd19f185d
ElsoMTLV <elsowatches@gmail.com>
2025-06-15 01:28:34 +0300

    Update ynet.py

--- sources/ynet.py
+++ sources/ynet.py
@@ -1,3 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
 
 def get_articles():
-    return [{"title": "Example from ynet", "url": "https://example.com/ynet"}]
+    url = "https://www.ynet.co.il/topics/מכבי_תל_אביב_בכדורסל"
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, "html.parser")
+    items = soup.select("article a")
+
+    articles = []
+    for item in items[:5]:
+        title = item.get_text(strip=True)
+        href = item.get("href")
+        if href and "/article/" in href:
+            link = "https://www.ynet.co.il" + href
+            articles.append({"title": title, "url": link})
+    return articles
