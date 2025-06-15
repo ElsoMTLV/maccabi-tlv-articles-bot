e028254df361ca74dc19fb6f6b6aee222efed9c5
ElsoMTLV <elsowatches@gmail.com>
2025-06-15 11:50:03 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,7 +1,14 @@
-
 import requests
 
 def send_article(token, chat_id, article):
     text = f"{article['title']}\n{article['url']}"
-    requests.post(f"https://api.telegram.org/bot{token}/sendMessage",
-                  data={"chat_id": chat_id, "text": text})
+    try:
+        response = requests.post(
+            f"https://api.telegram.org/bot{token}/sendMessage",
+            data={"chat_id": chat_id, "text": text}
+        )
+        print("Sent to Telegram:", article['title'], response.status_code)
+        if response.status_code != 200:
+            print("Telegram Error:", response.text)
+    except Exception as e:
+        print("Exception while sending Telegram message:", e)
