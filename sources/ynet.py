d5016b2f453a20ba1c76306b4811f7130e6553c8
ElsoMTLV <elsowatches@gmail.com>
2025-06-15 16:37:32 +0300

    Update ynet.py

--- sources/ynet.py
+++ sources/ynet.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_articles():
-    url = "https://www.ynet.co.il/topics/%D7%9E%D7%9B%D7%91%D7%99_%D7%AA%D7%9C_%D7%90%D7%91%D7%99%D7%91_%D7%91%D7%9B%D7%93%D7%95%D7%A8%D7%A1%D7%9C"
+    url = "https://tags.walla.co.il/%D7%9E%D7%9B%D7%91%D7%99_%D7%AA%D7%9C_%D7%90%D7%91%D7%99%D7%91_%D7%91%D7%9B%D7%93%D7%95%D7%A8%D7%A1%D7%9C"
     res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
     res.raise_for_status()
 
     soup = BeautifulSoup(res.text, "html.parser")
     articles = []
 
-    for item in soup.select("a.auto-list-link")[:10]:
+    for item in soup.select("article a")[:10]:
         link = item.get("href")
         title = item.get_text(strip=True)
 
@@ -17,7 +17,7 @@ def get_articles():
             continue
 
         if not link.startswith("http"):
-            link = "https://www.ynet.co.il" + link
+            link = "https://www.walla.co.il" + link
 
         articles.append({
             "title": title,
