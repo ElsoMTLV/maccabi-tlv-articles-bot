732441201a41c636167e09815f7d9286c51b86b9
ElsoMTLV <elsowatches@gmail.com>
2025-06-15 16:37:47 +0300

    Update walla.py

--- sources/walla.py
+++ sources/walla.py
@@ -2,16 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_articles():
-    url = "https://tags.walla.co.il/מכבי_תל_אביב_בכדורסל"
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, "html.parser")
-    items = soup.select("li.tag_item a.tag_link")
+    url = "https://tags.walla.co.il/%D7%9E%D7%9B%D7%91%D7%99_%D7%AA%D7%9C_%D7%90%D7%91%D7%99%D7%91_%D7%91%D7%9B%D7%93%D7%95%D7%A8%D7%A1%D7%9C"
+    res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    res.raise_for_status()
 
+    soup = BeautifulSoup(res.text, "html.parser")
     articles = []
-    for item in items[:5]:  # Limit to latest 5
+
+    for item in soup.select("article a")[:10]:
+        link = item.get("href")
         title = item.get_text(strip=True)
-        link = item["href"]
+
+        if not link or not title:
+            continue
+
         if not link.startswith("http"):
-            link = "https://tags.walla.co.il" + link
-        articles.append({"title": title, "url": link})
+            link = "https://www.walla.co.il" + link
+
+        articles.append({
+            "title": title,
+            "url": link
+        })
+
     return articles
