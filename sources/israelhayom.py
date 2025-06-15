520caf8749c240b91265fb3fea9b38cd144f0d5e
ElsoMTLV <elsowatches@gmail.com>
2025-06-15 16:36:51 +0300

    Update israelhayom.py

--- sources/israelhayom.py
+++ sources/israelhayom.py
@@ -2,14 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_articles():
-    url = "https://www.israelhayom.co.il/tag/מכבי-תל-אביב-בכדורסל"
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, "html.parser")
-    items = soup.select("div.compactArticle a.compactArticle_link")
+    url = "https://www.israelhayom.co.il/tag/%D7%9E%D7%9B%D7%91%D7%99-%D7%AA%D7%9C-%D7%90%D7%91%D7%99%D7%91-%D7%91%D7%9B%D7%93%D7%95%D7%A8%D7%A1%D7%9C"
+    res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    res.raise_for_status()
 
+    soup = BeautifulSoup(res.text, "html.parser")
     articles = []
-    for item in items[:5]:
+
+    for item in soup.select("div.article-teaser a.teaser-link")[:10]:
+        link = item.get("href")
         title = item.get_text(strip=True)
-        link = "https://www.israelhayom.co.il" + item["href"]
-        articles.append({"title": title, "url": link})
+
+        if not link or not title:
+            continue
+
+        if not link.startswith("http"):
+            link = "https://www.israelhayom.co.il" + link
+
+        articles.append({
+            "title": title,
+            "url": link
+        })
+
     return articles
